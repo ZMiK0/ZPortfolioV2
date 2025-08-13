221c757b3e24cc85b820937fe454d15c46bf7293
ZMiK0 <kbelz21z@gmail.com>
2025-08-13 02:49:41 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Code } from './pages/code/code';
+import { App } from './app';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: "home",
+        pathMatch: "full"
+    },
+    {
+        path:"home",
+        component:Home
+    },
+    {
+        path:"code",
+        component:Code
+    }
+];
