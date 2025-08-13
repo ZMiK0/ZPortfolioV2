2d081a1efdb6855a988a2478bce17c5901fae2fa
ZMiK0 <kbelz21z@gmail.com>
2025-08-13 02:25:12 +0200

    torture

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuButton } from "./items/menu-button/menu-button";
 
 @Component({
   selector: 'app-root',
-  imports: [MenuButton],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
